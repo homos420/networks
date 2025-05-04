dc00e98d160d25997becc9abc76cf306ac72e371
homos420 <chaabani.sidahmed1@gmail.com>
2025-05-04 11:12:10 +0100

    Delete Documents/net directory

--- Documents/net/object_oriented_programming/tps/heritage/heri/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main (String[] args){
-
-        pet chien = new pet();
-        chien.setColor("red");
-        chien.print();
-
-    }
-}
--- Documents/net/object_oriented_programming/tps/point2d/myMain.java
+++
@@ -1,7 +0,0 @@
-public class myMain {
-    public static void main (String[] args ){
-  point3d point = new point3d(3,4,5);
-        System.out.println(point.getX()+"//"+point.getY()+"//"+point.getZ());
-        System.out.println("distance ="+ point.distance());
-    }
-}
